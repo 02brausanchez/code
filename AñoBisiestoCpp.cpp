8d8406134f52365259d9a90f9e5acf4785cfdab2
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-14 09:16:47 -0600

    Add files via upload

---
+++ AñoBisiestoCpp.cpp
@@ -0,0 +1,26 @@
+# include <bits/stdc++.h>
+//# include <iostream>
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int a;
+    cout <<" Enter the year "<<endl;
+    cin >>a;
+    if(a % 4 == 0){
+        if(a % 100 == 0){
+            if(a % 400 == 0){
+                cout <<a<<" is a leap year "<<endl;
+            }else{
+                cout <<a<< " is not a leap year "<<endl;
+            }
+        }else{
+            cout <<a<<" is a leap year "<<endl;
+        }
+    }else{
+        cout <<a<<" is not a leap year "<<endl;
+    }
+    
+    return 0;
+}
