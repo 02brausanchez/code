b1239a06d513d907c093df797bcae4cce08a31b9
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-14 00:27:23 -0600

    Add files via upload

---
+++ cppholamundo.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ ios_base::sync_with_stdio(0); cin.tie(0);
+ random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1,50);
+    int num, contP, contI, total;
+    contP = 0; contI = 0;
+    cout <<" Cuantos numeros quieres generar? " <<endl;
+    cin >> total;
+    cout <<""<<endl;
+    while (total > 0){
+        num = dist(gen);
+        cout <<"" <<num <<endl;
+        if(num % 2 == 0){
+            contP++;
+        }else{
+            contI++;
+        }
+        total--;
+
+    }
+    cout <<" hubo "<< contP <<" pares"<<endl;
+    cout <<" hubo "<< contI <<" impares"<<endl;
+
+    return 0;
+}
