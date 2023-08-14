520cb23fd5a8243e2a89c5b2d7c51d0326dd51dc
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-14 09:29:47 -0600

    Update The hour on a distant planet.cpp

--- The hour on a distant planet.cpp
+++ The hour on a distant planet.cpp
@@ -26,51 +26,5 @@ int main() {
 }
 
 
-/*
-   
-   dias =  num / 12; 
-   horas =  num % 12; 
-   minutos = num % 70;
-   num /= 70;  
-   segundos =  num % 50; 
-   num /= 50;
-   
-   segundos  = num % 50; 
-   //num /= 50; 
-   
-   minutos = num % 70; 
-   //num /= 70; 
 
-   horas = num % 12; 
-   dias = num / 12; 
-  */  
- //  cout << dias << horas << minutos << segundos <<endl; 
-
-
-  /*
-    days = (num * 12);
-    cout << days <<endl;
-    hours = (days * 70);
-    cout << hours <<endl;  
-    minutes = (num * 70) / hours;
-    cout << minutes <<endl;  
-    //seconds = num (minutes * 50);
-    //cout <<seconds <<endl;
-    //cout << days<<""<< hours <<""<< minutes <<""<< seconds<<""<<endl;
-    */
-   /*
-    dias = num * 0.00000578704 ; 
-    cout <<dias <<endl;
-    horas = num * 0.00028571429;
-    cout <<horas<<endl; 
-    minutos = num * 0.02;
-    cout << minutos <<endl; 
-    cout << dias <<""<< horas <<""<< minutos <<endl; 
-    */
-
-   
-   //lli days, hours , minutes , seconds;
-   // days =  12;//horas 
-   // hours = 70;//minutos
-   // minutes = 50;//segundos 
 
