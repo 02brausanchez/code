c4e4dc068f74bcdaa1131c0f0b0e3e23fabddcad
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-14 09:27:07 -0600

    Add files via upload

---
+++ EvaluandoFormulas.cpp
@@ -0,0 +1,25 @@
+
+/* 
+    Formulas a evaluar: 
+    y = x + 5 / 2(x + 1 ) 
+    z = y*y + x ( x - 2y) / xy
+    
+    Calcular el valor de z dado x 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    double x , y , z; 
+    //double EformY ; double EformZ
+    cin >> x; 
+    y = (x + 5) / (2 * ( x + 1)); 
+    z =  (pow( y , 2) + x) * ( x - 2 * y) / (x * y); 
+    cout << z <<endl; 
+// 5 >= 
+
+    return 0;
+}
