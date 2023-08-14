81b9886de7293c13a17ac12c9d1a39c8f11bf123
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-14 09:26:21 -0600

    Add files via upload

---
+++ CalculosMentales.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    double R; double sum; double sqrt; double div; double cub; 
+    cin >> R; 
+    cout <<fixed<< R <<endl;
+    sum = R + 5; 
+    cout <<fixed<< sum <<endl;
+    sqrt = pow( sum , 2 );
+    cout <<fixed<< sqrt <<endl;
+    div = (sqrt / (R / 3 ));
+    cout <<fixed<< div <<endl; 
+    cub = pow(div,3); 
+    cout <<fixed<< cub <<endl; 
+    
+    //cout << sum << sqrt << div << cub <<endl;
+    // la palabra fixed sirve para que tal cual muestre los numeros sin 
+    // la notacion cientifica y ayude verlo de una manera menos compacta 
+
+    return 0;
+}
