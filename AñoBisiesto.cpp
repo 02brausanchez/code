8e41717f25570ec8a4ca685619068bbcd4ed6786
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-15 01:12:26 -0600

    Update AñoBisiesto.cpp

--- AñoBisiesto.cpp
+++ AñoBisiesto.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int a;
-    cout <<" Enter the year "<<endl;
+    cout <<" enter year "<<endl;
     cin >>a;
     if(a % 4 == 0){
     if(a % 100 == 0){
