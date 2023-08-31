2cf4667b4a6a2e15c334ae0dd8fd213296828957
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-08-30 21:32:16 -0600

    Add files via upload

---
+++ ConteoBasico.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n1, n2, cont;
+    cin >> n1 >> n2; 
+    cont = n1;           
+    do{                  //while(cont <= n2);               
+    cout << cont << endl;//cout << cont << endl;       
+    cont++;              //cont++;                      
+    }while(cont <=n2);
+    
+    return 0;
+}
