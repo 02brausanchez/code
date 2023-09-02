c06388e09e04c437392b5b68d918a6c26546191e
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-09-01 22:35:59 -0600

    Update AñoBisiesto.cpp

--- AñoBisiesto.cpp
+++ AñoBisiesto.cpp
@@ -1,23 +1,24 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
+
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int a;
-    cout <<" enter year "<<endl;
+    cout <<" Enter the year "<<endl;
     cin >>a;
     if(a % 4 == 0){
-    if(a % 100 == 0){
-    if(a % 400 == 0){
-    cout <<a<<" is a leap year "<<endl;
+        if(a % 100 == 0){
+            if(a % 400 == 0){
+                cout <<a<<" is a leap year "<<endl;
+            }else{
+                cout <<a<< " is not a leap year "<<endl;
+            }
+        }else{
+            cout <<a<<" is a leap year "<<endl;
+        }
     }else{
-    cout <<a<< " is not a leap year "<<endl;
-    }
-    }else{
-    cout <<a<<" is a leap year "<<endl;
-    }
-    }else{
-    cout <<a<<" is not a leap year "<<endl;
+        cout <<a<<" is not a leap year "<<endl;
     }
     
     return 0;
