1fd932e1c280aaf2c754ec775a4289f83aea2822
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-09-06 01:01:48 -0600

    Update AñoBisiesto.cpp

--- AñoBisiesto.cpp
+++ AñoBisiesto.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main(){
-
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int a;
     cout <<" Enter the year "<<endl;
@@ -23,3 +22,5 @@ int main(){
     
     return 0;
 }
+
+
